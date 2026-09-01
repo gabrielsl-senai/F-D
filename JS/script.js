3e29ccc34ef9c31270c9340fc0abdc02ab0a9448
gabriel senai <gabriel_as_lima@estudante.sesisenai.org.br>
2026-08-31 21:45:06 -0300

    commit 4 (corrigir amanhã)

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+function abrir(){
+    document.getElementById("capa-tela").style.display = "none";
+    document.getElementById("tela-lobby").style.display = "block";
+
+}
+function fechar(){
+    document.getElementById("tela-lobby").style.display = "none";
+    document.getElementById("capa-tela").style.display = "block";
+
+}
